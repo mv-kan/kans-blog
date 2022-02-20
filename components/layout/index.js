f277e5c00091a1b13a1b35b6949968e8a5f81988
michael <ittybemain@gmail.com>
2022-02-20 15:11:54 +0100

    home page (without desktop version of contacts)

--- components/layout/index.js
+++ components/layout/index.js
@@ -2,32 +2,14 @@ import Head from "next/head"
 import Header from '../header'
 import { useState } from 'react'
 import styles from './layout.module.css'
-export default function Layout() {
+export default function Layout({children}) {
     return (
         <div className={styles.layout}>
             <Head>
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400&family=Roboto+Slab:wght@400;500&family=Roboto:wght@400;500&display=swap" rel="stylesheet"></link>
             </Head>
             <Header></Header>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
+            {children}
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,66 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import Layout from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import styles from '../styles/home.module.css'
+import profilePicture from '../public/images/profile.jpg';
+
+const ContactLink = ({ text, iconName, href }) => {
+  return (
+    <a href={href} className={styles.contactLink}>
+      <div>
+        <span className='material-icons-round'>{iconName}</span>
+        <span>{text}</span>
+      </div>
+    </a>
+  )
+}
+
 export default function Home() {
   return (
-    <>
-      <Layout></Layout>
-    </>
+    <Layout>
+      <div className={utilStyles.layoutContent}>
+        <section className={styles.section}>
+          <h2>
+            Greetings!
+          </h2>
+          <span>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit quas iste quaerat porro eos earum repudiandae fuga, ipsum ea a aperiam quo eligendi ducimus quis, suscipit tempora, blanditiis maxime omnis.
+          </span>
+        </section>
+        <section className={styles.section}>
+          <div className={styles.profilePictureWrapper}>
+            <Image
+              src={profilePicture}
+            />
+          </div>
+          <h4>Michael Kan</h4>
+          <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit quas iste quaerat porro eos earum repudiandae fuga, ipsum ea a aperiam quo eligendi ducimus quis, suscipit tempora, blanditiis maxime omnis.</span>
+        </section>
+      </div>
+      <section className={`${styles.contacts}`}>
+        <h4 className={styles.contactsHeader}>Contacts</h4>
+        <ul className={styles.listOfContacts}>
+          <li>
+            <ContactLink
+              text={'michael.kan.mail@gmail.com'}
+              iconName={'mail'}
+              href={'mailto:michael.kan.mail@gmail.com'} />
+          </li>
+          <li>
+            <ContactLink
+              text={'michael.kan.mail@gmail.com'}
+              iconName={'mail'}
+              href={'mailto:michael.kan.mail@gmail.com'} />
+          </li>
+          <li>
+            <ContactLink
+              text={'michael.kan.mail@gmail.com'}
+              iconName={'mail'}
+              href={'mailto:michael.kan.mail@gmail.com'} />
+          </li>
+        </ul>
+      </section>
+    </Layout>
   )
 }

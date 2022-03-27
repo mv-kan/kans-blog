29a3092b2fda9eb589478ac10dacc4b5f4df4dc1
michael <ittybemain@gmail.com>
2022-03-27 13:44:20 +0200

    no api, search will be implemented on page

--- pages/api/posts/index.js
+++
@@ -1,7 +0,0 @@
-import { getAllPostsIds, getPostData} from "../../../lib/post";
-
-const postsIds = getAllPostsIds()
-
-export default function Posts(req, res) {
-    res.status(200).json({...postsIds})
-}

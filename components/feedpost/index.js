b4f4202f66061bfe5ee0cc0564a3b4685c5787f2
michael <ittybemain@gmail.com>
2022-04-01 18:18:41 +0200

    feed post styles

---
+++ components/feedpost/index.js
@@ -0,0 +1,31 @@
+import styles from "./feedpost.module.css"
+import Link from "next/link"
+
+export default function Feedpost({title, description, tags, publishedOn, updatedOn, id}) {
+    const tagList = tags.map((tag, i) => {
+        return <span key={i} className={styles.tag}>{tag}</span>
+    })
+    
+    if (description.length > 100) {
+        description = description.slice(80) + "..."
+    }
+
+    return (
+        <Link href={`blog/${id}`}>
+            <div className={styles.feedpost}>
+                <div className={styles.heading}>
+                    <span className={styles.title}>{title}</span>
+                    <span className={styles.publishedOn}>{publishedOn}</span>
+                </div>
+                <div className={styles.description}>
+                    {description}
+                </div>
+                <span className={styles.tagList}>
+                    {
+                        tagList
+                    }
+                </span>
+            </div>
+        </Link>
+    );
+}
--- pages/blog.js
+++ pages/blog.js
@@ -1,9 +1,20 @@
 import Layout from '../components/layout'
+import Feedpost from '../components/feedpost'
+import utilStyles from '../styles/utils.module.css' 
 
 export default function Blog() {
     return (
         <Layout>
-            
+            <div className={utilStyles.layoutContent}>
+                <Feedpost
+                    id={"cake-is-a-lie"}
+                    title={"Test title"}
+                    description={"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus, sequi aliquam quam fugit laboriosam aspernatur quas veritatis blanditiis alias provident, sit in numquam eius illo facere rerum nostrum suscipit dolorum."}
+                    tags={["first tag", "second tag", "third tag"]}
+                    publishedOn={"01/02/2020"}
+                    updatedOn={"04/02/2020"}
+                />
+            </div>
         </Layout>
     )
 }
